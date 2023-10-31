9ec150df7b987032018d5b13fdecec61f520d843
109310307 <146065460+109310307@users.noreply.github.com>
2023-10-31 14:39:38 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+//hw5-2
+int main()
+{
+int min;
+double money;
+scanf("%d", &min);
+
+if(min <= 800)
+    money = min * 0.9;
+else if(min > 800 && min < 1500)
+    money = min * 0.9 * 0.9;
+else if(min >= 1500)
+    money = min * 0.9 * 0.79;
+
+money = (int)((money * 10) + 0.5) / 10.0;
+printf("%.1f\n",money);
+}
