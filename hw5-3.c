8a0829e2164e5a6bf568db7f22c71dcdbe570d49
109310307 <146065460+109310307@users.noreply.github.com>
2023-10-31 14:40:09 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+//hw5-3
+int main()
+{
+int hour, hour_money;
+double money;
+scanf("%d %d", &hour, &hour_money);
+
+if(hour <= 60)
+    money = hour * hour_money;
+else if(hour >= 61 && hour <= 120)
+    money = 60 * hour_money + (hour - 60) * hour_money * 1.33;
+else if(hour >= 121)
+    money = 60 * hour_money + 60 * hour_money * 1.33 + (hour - 120) * hour_money * 1.66;
+
+printf("%.1f\n",money);
+}
